592d949a77e482385edfd935dfcafd2affe96b89
preety.mehra <preety.mehra@thoughtworks.com>
2022-03-22 14:27:54 +0530

    ADD enum Supermarket2 and unitPrice of each product

--- src/main/java/Supermarket2.java
+++ src/main/java/Supermarket2.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public enum Supermarket2 {
+public enum Supermarket2 {
+    BREAD_UNIT_PRICE(10),
+    RICE_UNIT_PRICE(62),
+    SUGAR_UNIT_PRICE(41);
+
+    private final double unittPrice;
+
+    Supermarket2(int unitPrice) {
+        this.unittPrice = unitPrice;
+    }
 }
