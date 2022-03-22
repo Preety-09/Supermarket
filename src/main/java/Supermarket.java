73d272a46f1c69d79f7660a6f2a7d31aa2ef6b22
preety.mehra <preety.mehra@thoughtworks.com>
2022-03-22 13:46:54 +0530

    ADD findSupermarketWithCheapestPrice method

--- src/main/java/Supermarket.java
+++ src/main/java/Supermarket.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Supermarket {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Supermarket {
+    private final Bread bread;
+    private final Rice rice;
+    private final Sugar sugar;
+
+    public Supermarket(Bread bread, Rice rice, Sugar sugar) {
+        this.bread = bread;
+        this.rice = rice;
+        this.sugar = sugar;
+    }
+
+    public List<String> findSupermarketWithCheapestPrice() {
+        List<String> listOfSupermarketWithCheapestPrice = new ArrayList<>();
+        return listOfSupermarketWithCheapestPrice;
+    }
 }
