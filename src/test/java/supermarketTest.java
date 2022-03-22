40507fd39315dbe8e46dfa669f8ef8bd86596196
preety.mehra <preety.mehra@thoughtworks.com>
2022-03-22 14:31:19 +0530

    REFACTOR add quantity to each product's constructor

--- src/test/java/supermarketTest.java
+++ src/test/java/supermarketTest.java
@@ -8,9 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class supermarketTest {
     @Test
     void returnSupermarketThatProvidesCheapestPrice() {
-        Bread bread = new Bread();
-        Rice rice = new Rice();
-        Sugar sugar = new Sugar();
+        Bread bread = new Bread(2);
+        Rice rice = new Rice(10);
+        Sugar sugar = new Sugar(5);
         Supermarket supermarket = new Supermarket(bread, rice, sugar);
 
         List<String> listOfSupermarketWithCheapestPrice = supermarket.findSupermarketWithCheapestPrice();
