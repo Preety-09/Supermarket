bd57d2cbf4b964432bca2a84c818586e81c26b8c
preety.mehra <preety.mehra@thoughtworks.com>
2022-03-22 14:32:34 +0530

    REFACTOR add constructor with parameter quantity

--- src/main/java/Rice.java
+++ src/main/java/Rice.java
@@ -1,2 +1,7 @@
 public class Rice {
+    private final double quantity;
+
+    public Rice(double quantity) {
+        this.quantity = quantity;
+    }
 }
--- src/main/java/Sugar.java
+++ src/main/java/Sugar.java
@@ -1,2 +1,7 @@
 public class Sugar {
+    private final double quantity;
+
+    public Sugar(double quantity) {
+        this.quantity = quantity;
+    }
 }
