9143d90f681a12e25669aa567b25a693850c6697
preety.mehra <preety.mehra@thoughtworks.com>
2022-03-23 20:59:25 +0530

    ADD implementation to returnSupermarketThatProvidesCheapestPriceForAllProducts

--- src/test/java/supermarketTest.java
+++ src/test/java/supermarketTest.java
@@ -2,7 +2,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class supermarketTest {
@@ -22,6 +23,14 @@ public class supermarketTest {
 
     @Test
     void returnSupermarketThatProvidesCheapestPriceForAllProducts() {
+        Bread bread = new Bread(2);
+        Rice rice = new Rice(10);
+        Sugar sugar = new Sugar(5);
+        Supermarket supermarket = new Supermarket(bread, rice, sugar);
+
+        String supermarketWithCheapestPriceForAllProducts = supermarket.findSupermarketWithCheapestPriceForAllProducts();
+
+        assertThat(supermarketWithCheapestPriceForAllProducts, is("Supermarket2"));
 
     }
 }
