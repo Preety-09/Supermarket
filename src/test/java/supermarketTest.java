f5ced3e37b057fa5b948f468d1b32aaa990312a0
preety.mehra <preety.mehra@thoughtworks.com>
2022-03-23 20:32:55 +0530

    REFACTOR method name to returnSupermarketThatProvidesCheapestPriceForEachProduct

--- src/test/java/supermarketTest.java
+++ src/test/java/supermarketTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class supermarketTest {
     @Test
-    void returnSupermarketThatProvidesCheapestPrice() {
+    void returnSupermarketThatProvidesCheapestPriceForEachProduct() {
         Bread bread = new Bread(2);
         Rice rice = new Rice(10);
         Sugar sugar = new Sugar(5);
         Supermarket supermarket = new Supermarket(bread, rice, sugar);
 
-        List<String> listOfSupermarketWithCheapestPrice = supermarket.findSupermarketWithCheapestPrice();
+        List<String> listOfSupermarketWithCheapestPrice = supermarket.findSupermarketWithCheapestPriceForEachProduct();
         List<String> expectedListOfSupermarketWithCheapestPrice = Arrays.asList("Supermarket2", "Supermarket1", "Supermarket1");
 
         assertEquals(expectedListOfSupermarketWithCheapestPrice, listOfSupermarketWithCheapestPrice);
