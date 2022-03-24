1d5a35c902495364e82af77dd2000c5bca49533b
preety.mehra <preety.mehra@thoughtworks.com>
2022-03-24 11:08:42 +0530

    ADD calculateTotalPriceOfProductsFromSupermarket1 method

--- src/main/java/Supermarket1.java
+++ src/main/java/Supermarket1.java
@@ -5,8 +5,14 @@ public enum Supermarket1 {
 
     private final double unitPrice;
 
-    Supermarket1(int unitPrice) {
+    Supermarket1(double unitPrice) {
         this.unitPrice = unitPrice;
+    }
+
+    public static double calculateTotalPriceOfProductsFromSupermarket1(double bread_quantity, double rice_quantity, double sugar_quantity) {
+        return (bread_quantity * BREAD_UNIT_PRICE.getUnitPrice() +
+                rice_quantity * RICE_UNIT_PRICE.getUnitPrice() +
+                sugar_quantity * SUGAR_UNIT_PRICE.getUnitPrice());
     }
 
     public double getUnitPrice() {
