d7a114e8344dc39bfecad376bc1236659baf2946
preety.mehra <preety.mehra@thoughtworks.com>
2022-03-24 12:15:41 +0530

    ADD calculateTotalPriceOfProductsIncludingServiceChargeFromSupermarket1 method

--- src/main/java/Supermarket1.java
+++ src/main/java/Supermarket1.java
@@ -1,7 +1,8 @@
 public enum Supermarket1 {
     BREAD_UNIT_PRICE(15),
     RICE_UNIT_PRICE(60),
-    SUGAR_UNIT_PRICE(40);
+    SUGAR_UNIT_PRICE(40),
+    SUPERMARKET1_SERVICE_CHARGE(0.1);
 
     private final double unitPrice;
 
@@ -13,6 +14,11 @@ public enum Supermarket1 {
         return (bread_quantity * BREAD_UNIT_PRICE.getUnitPrice() +
                 rice_quantity * RICE_UNIT_PRICE.getUnitPrice() +
                 sugar_quantity * SUGAR_UNIT_PRICE.getUnitPrice());
+    }
+
+    public static double calculateTotalPriceOfProductsIncludingServiceChargeFromSupermarket1(double bread_quantity, double rice_quantity, double sugar_quantity) {
+        return (calculateTotalPriceOfProductsFromSupermarket1(bread_quantity, rice_quantity, sugar_quantity) +
+                   (calculateTotalPriceOfProductsFromSupermarket1(bread_quantity, rice_quantity, sugar_quantity) * SUPERMARKET1_SERVICE_CHARGE.getUnitPrice()));
     }
 
     public double getUnitPrice() {
